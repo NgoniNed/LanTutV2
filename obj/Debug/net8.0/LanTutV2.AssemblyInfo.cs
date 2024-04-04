fa79b6fafffdc2b15c2a9be2d2098277a634dfcd
Ngonidyashe Nedziwe <ressned@gmail.com>
2024-04-04 17:52:18 +0200

    updated profile page to have a profile nav menu

--- obj/Debug/net8.0/LanTutV2.AssemblyInfo.cs
+++ obj/Debug/net8.0/LanTutV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanTutV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37765238459f07656b905d1e35172cfe6f06cb4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d769324d816d0c9294aee54f583f7bfca307e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanTutV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanTutV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
